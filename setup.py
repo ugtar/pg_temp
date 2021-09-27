532c9ecba755c3f17e85645e6123462393b34519
Uri Okrent <uokrent@gmail.com>
2021-09-27 11:25:33 -0400

    drop official support for py3.5, add 3.9
    Signed-off-by: Uri Okrent <uokrent@gmail.com>

--- setup.py
+++ setup.py
@@ -32,10 +32,10 @@ SETUP_ARGS = dict(
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
